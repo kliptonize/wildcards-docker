668c3498b0f1a737f0f880799e19fc7a0daeb765
Koen <koen@beformed.be>
2017-09-02 13:52:25 +0200

    Initial commit - yay

---
+++ README.md
@@ -0,0 +1,7 @@
+# Docker for Wildcards
+As you need certain things to start developing on the Wildcards-base, I made a Docker-repo.
+
+## How to use
+1) Clone
+2) Run init.sh
+3) Profit
---
+++ init.sh
@@ -0,0 +1,17 @@
+cd containers
+if [ -d wildcards-api ]; then
+	cd wildcards-api
+	git checkout develop
+	cd ..
+else
+	git clone -b develop https://github.com/kliptonize/wildcards-api.git
+fi
+if [ -d wildcards-cms ]; then
+	cd wildcards-cms
+	git checkout develop
+	cd ..
+else
+	git clone -b develop https://github.com/kliptonize/wildcards-cms.git
+fi
+cd ..
+docker-compose up --build
