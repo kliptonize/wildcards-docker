439129332a7b23694cfa271780576612a16cc6a0
Koen <koen@beformed.be>
2017-09-05 23:21:03 +0200

    Added git pull to init.sh
    You kind of need that..to get the latest version of the containers.. #whoopz

--- init.sh
+++ init.sh
@@ -2,6 +2,7 @@ cd containers
 if [ -d wildcards-api ]; then
 	cd wildcards-api
 	git checkout develop
+	git pull
 	cd ..
 else
 	git clone -b develop https://github.com/kliptonize/wildcards-api.git
@@ -9,6 +10,7 @@ fi
 if [ -d wildcards-cms ]; then
 	cd wildcards-cms
 	git checkout develop
+	git pull
 	cd ..
 else
 	git clone -b develop https://github.com/kliptonize/wildcards-cms.git
